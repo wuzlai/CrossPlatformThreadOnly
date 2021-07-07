fa71fc56cc36a6e00447e9e1bc1d1d337b11522c
wuzl <mes9@mesince.com>
2021-07-07 13:48:52 +0800

    threadonly

---
+++ CrossPlatformThreadOnly/FileLocks.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace CrossPlatformThreadOnly
+{
+    /// <summary>
+    /// 文件锁
+    /// </summary>
+    public class FileLocks
+    {
+        static FileStream _lockFile;
+
+        /// <summary>
+        /// 检测实例唯一
+        /// </summary>
+        /// <returns></returns>
+        public bool CheckSingleInstance()
+        {
+            //判断  MacOS系统自带实例检测， 只考虑 Windows和linux下的实例唯一 检测
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                return true;
+            }
+
+            var dir = AppDomain.CurrentDomain.BaseDirectory;
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            try
+            {
+                //Console.WriteLine("Check File Lock");
+                _lockFile = File.Open(Path.Combine(dir, ".lock"), FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+                _lockFile.Lock(0, 0);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("error:" + ex.Message);
+                return false;
+            }
+        }
+    }
+}
---
+++ NugetThreadOnly/Program.cs
@@ -0,0 +1,27 @@
+﻿using CrossPlatformThreadOnly;
+using System;
+using System.Runtime.InteropServices;
+
+namespace NugetThreadOnly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            #region 实例唯一
+            if (!new FileLocks().CheckSingleInstance())
+            {
+                Console.WriteLine("Another instance of application is running. Closing.");
+                Environment.Exit(0);
+            }
+            #endregion
+
+            Console.WriteLine("End World!");
+
+            Console.ReadKey();
+
+        }
+    }
+}
